ee5a46efbd8815f120fdd9632e982d204e3ced0a
Grandhe Harshavardhan <chintusunny9898@gmail.com>
2025-12-25 12:42:25 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+        System.out.println("I am Harsha");
+        System.out.println("This is my first programme");
+        System.out.print("In java");
+    }
+    
+}
---
+++ backslash.java
@@ -0,0 +1,12 @@
+public class backslash {
+    public static void main (String[] args) {
+        System.out.println("Hello \nRaja");
+        System.out.println("Hello \\Raja\\");
+        System.out.println("Hello \"Raja\"");
+        System.out.println("Hello \t Raja");
+        System.out.println("Hello \bRaja");
+        System.out.println("Hello Raja");
+        System.out.println("Hello\bRaja");
+    }
+    
+}
