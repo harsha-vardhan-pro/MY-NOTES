419b658232e48cb93b438bc6d7bb90051326c9b7
Grandhe Harshavardhan <chintusunny9898@gmail.com>
2025-12-26 13:00:58 +0530

    add file

--- user_input.java
+++ user_input.java
@@ -1,6 +1,10 @@
 //here "import java.util.Scanner" is necessary
 //because Scanner is an inbuilt function in java to initialize it
 // we must use this code to take the input.
+//after Scanner in the start we have to give the code as it is so that we can give input
+//String gives the type of variable,and name we can change it
+//input.nextLine() makes your input to be shown in the next line of input
+
 import java.util.Scanner;
 public class user_input {
     public static void main(String[] args){
